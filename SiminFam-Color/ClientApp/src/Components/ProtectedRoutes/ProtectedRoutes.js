81e429f98bedfcf1ebd1129b916733cc3fd7cea2
IT2 <IT2@IT2>
2020-09-19 14:23:20 +0430

    protected routes minor change

--- SiminFam-Color/ClientApp/src/Components/ProtectedRoutes/ProtectedRoutes.js
+++ SiminFam-Color/ClientApp/src/Components/ProtectedRoutes/ProtectedRoutes.js
@@ -1,9 +1,10 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useEffect } from 'react';
 import ErrorBoundary from '../../Shared/ErrorBoundary/ErrorBoundary'
 import { Switch, Route, withRouter } from 'react-router-dom';
 import ErrorPage from '../../Shared/ErrorPage/ErrorPage';
 import TopNavBar from '../../UI/Navbar/TopNavBar/TopNavbar'
 import Loading from '../../UI/Loading/Loading';
+import { user } from '../../Services/User.service';
 
 const AddNew = lazy(() => import('../AddNewColor/AddNew'));
 const Setting = lazy(() => import('../Setting/Setting'))
@@ -14,6 +15,18 @@ const LoadingElement = <div className="text-center">
 
 
 const ProtectedRoutes = props => {
+    useEffect(() => {
+        console.log('PRotected Routes')
+        user.checkLogin().then(data => {
+            if (data) {
+                props.history.replace('/main/addnew')
+                //props.storeUserData(data.data)
+            } else {
+                user.logout()
+                props.history.replace('/login')
+            }
+        })
+    }, [])
     return (
         <React.Fragment>
             <TopNavBar />
